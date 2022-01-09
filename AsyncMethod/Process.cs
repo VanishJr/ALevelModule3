b02123b8fd56e88f905f1b5fbecac7efce4f3372
Ivan Logutov <foreverivan2004@gmail.com>
2022-01-10 00:18:37 +0200

    create an async method

---
+++ AsyncMethod/Process.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncMethod
+{
+    class Process
+    {
+
+        public async Task<string> HelloAsync()
+        {
+            string Hellotxt = null;
+            using (StreamReader reader = File.OpenText(@"D:\Hello.txt"))
+            {
+
+                string s = "";
+                while ((s = await reader.ReadLineAsync()) != null)
+                {
+                    Hellotxt += s + "\n";
+                    //Console.WriteLine(s);
+                }
+            }
+            return Hellotxt;
+        }
+        public async Task<string> WorldAsync()
+        {
+            string Worldtxt = null;
+            using (StreamReader reader = File.OpenText(@"D:\World.txt"))
+            {
+                string s = "";
+                while ((s = await reader.ReadLineAsync()) != null)
+                {
+                    Worldtxt += s + "\n";
+                }
+            }
+            return Worldtxt;
+        }
+        public async Task<string> TwoMethods()
+        {
+            string Hellotxt = await Task.Run(() => HelloAsync());
+            string Worldtxt = await Task.Run(() => WorldAsync());
+            string[] HT = Hellotxt.Split("\n");
+            string[] WT = Worldtxt.Split("\n");
+            string result = null;
+            foreach (var worlds in HT)
+            {
+                foreach (var other in WT)
+                {
+                    result += worlds + other;
+                }
+            }
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
+
---
+++ AsyncMethod/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AsyncMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                numbers.Add(i);
+            }
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num + " ");
+            }
+            Console.WriteLine("\nAddRange");
+
+            numbers.AddRange(numbers);
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num + " ");
+            }
+            Console.WriteLine("\nRemove");
+            numbers.Remove(2);
+
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num + " ");
+            }
+            Console.WriteLine("\nRemoveAt");
+            numbers.RemoveAt(1);
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num + " ");
+            }
+            Console.WriteLine("\nSort");
+            numbers.Sort();
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num + " ");
+            }
+            Process obj = new Process();
+            _ = obj.TwoMethods();
+        }
+    }
+}
