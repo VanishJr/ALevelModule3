41437f0c0a886dd257571a03fe21f42f5d710715
Vanish <91227368+VanishJr@users.noreply.github.com>
2022-02-22 16:45:12 +0200

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture02
+{
+    class Class2
+    {
+        private int res;
+        public delegate int GetPow(int a, int b);
+        public bool Calc()
+        {
+            var obj = new Class1();
+            int a = 2, b = 3;
+            GetPow getint = obj.pow;
+            res = getint.Invoke(a, b);
+            Console.WriteLine(res);
+            return true;
+
+        }
+        public bool Result(int c = 2)
+        {
+            Calc();
+            if (res % c == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lecture02
+{
+    class Program
+    {
+        static void Main()
+        {
+            var obj = new Program();
+            obj.Show();
+        }
+
+
+        public int Show()
+        {
+            Class2 obj = new Class2();
+            Console.WriteLine(obj.Result(3));
+            return 0;
+        }
+    }
+}
